55485a6186328552f4abfdb8855b101a9a43972b
Andy <zy_try@live.cn>
2025-04-02 13:46:22 +0800

    Update getSnowID.cs
    版本适配

--- Sqlserver/getSnowID.cs
+++ Sqlserver/getSnowID.cs
@@ -7,17 +7,13 @@ using Microsoft.SqlServer.Server;
 
 public partial class UserDefinedFunctions
 {
-    private static readonly Dictionary<string, IdWorker> pools = new Dictionary<string, IdWorker>();
+    private static readonly Dictionary<string, IdWorker> pools = new();
 
-    [Microsoft.SqlServer.Server.SqlFunction]
-    public static SqlString getSnowID(SqlInt32 workerId,SqlInt32 datacenterId)
+    [SqlFunction]
+    public static SqlInt64 getSnowID(SqlInt32 workerId,SqlInt32 datacenterId)
     {
         var key = workerId.Value + "-" + datacenterId.Value;
-        if (!pools.ContainsKey(key))
-        {
-            pools.Add(key, new IdWorker(workerId.Value, datacenterId.Value));
-        }
-        var result = pools[key].NextId();
-        return new SqlString(result.ToString());
+        if (!pools.ContainsKey(key)) pools.Add(key, new(workerId.Value, datacenterId.Value));
+        return new SqlInt64(pools[key].NextId());
     }
 }
