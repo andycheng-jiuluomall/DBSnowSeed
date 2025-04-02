768001f1783c330a65182fa735868e1ac5657a72
Andy <zy_try@live.cn>
2025-04-02 13:59:29 +0800

    1
    1

--- Sqlserver/getSnowID.cs
+++ Sqlserver/getSnowID.cs
@@ -10,10 +10,10 @@ public partial class UserDefinedFunctions
     private static readonly Dictionary<string, IdWorker> pools = new();
 
     [SqlFunction]
-    public static SqlInt64 getSnowID(SqlInt32 workerId,SqlInt32 datacenterId)
+    public static SqlInt64 SnowSeed(SqlInt32 workerId,SqlInt32 datacenterId)
     {
         var key = workerId.Value + "-" + datacenterId.Value;
         if (!pools.ContainsKey(key)) pools.Add(key, new(workerId.Value, datacenterId.Value));
-        return new SqlInt64(pools[key].NextId());
+        return new(pools[key].NextId());
     }
 }
