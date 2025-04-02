2718f0ea9fd9f901b00463055f1b2a1cdabb7db2
Andy <zy_try@live.cn>
2025-04-02 14:07:17 +0800

    1
    1

--- Sqlserver/ClassFiles.cs
+++ Sqlserver/ClassFiles.cs
@@ -1,20 +1,4 @@
 using System;
-public class DisposableAction : IDisposable
-{
-    readonly Action _action;
-
-    public DisposableAction(Action action)
-    {
-        if (action == null)
-            throw new ArgumentNullException("action");
-        _action = action;
-    }
-
-    public void Dispose()
-    {
-        _action();
-    }
-}
 public class IdWorker
 {
     //基准时间
