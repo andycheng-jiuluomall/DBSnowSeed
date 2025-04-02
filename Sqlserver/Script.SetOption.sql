8e43fbabce4b4a9fcd74a3d5e64b0b48faaf2f0a
Andy <zy_try@live.cn>
2025-04-02 13:37:50 +0800

    sqlserver
    sqlserver

---
+++ Sqlserver/Script.SetOption.sql
@@ -0,0 +1,26 @@
+﻿/*
+ 预先部署脚本模板							
+--------------------------------------------------------------------------------------
+ 此文件包含将在生成脚本之前执行的 SQL 语句。	
+ 使用 SQLCMD 语法将文件包含在预先部署脚本中。			
+ 示例:      :r .\myfile.sql								
+ 使用 SQLCMD 语法引用预先部署脚本中的变量。		
+ 示例:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+Use [$(DatabaseName)]
+
+select * from sys.configurations where name='clr enabled';
+go
+sp_configure 'show advanced options',1
+GO
+RECONFIGURE
+GO
+sp_configure 'clr enabled',1
+GO
+RECONFIGURE
+GO
+alter database [$(DatabaseName)] set trustworthy on
+
+exec sp_changedbowner 'sa'
---
+++ Sqlserver/getSnowID.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using Microsoft.SqlServer.Server;
+
+public partial class UserDefinedFunctions
+{
+    private static readonly Dictionary<string, IdWorker> pools = new Dictionary<string, IdWorker>();
+
+    [Microsoft.SqlServer.Server.SqlFunction]
+    public static SqlString getSnowID(SqlInt32 workerId,SqlInt32 datacenterId)
+    {
+        var key = workerId.Value + "-" + datacenterId.Value;
+        if (!pools.ContainsKey(key))
+        {
+            pools.Add(key, new IdWorker(workerId.Value, datacenterId.Value));
+        }
+        var result = pools[key].NextId();
+        return new SqlString(result.ToString());
+    }
+}
